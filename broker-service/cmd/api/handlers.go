e8e1e60ed0aafa5694cc1adf21d6ef3507f27f4c
INegmetov <negmetov.i@gmail.com>
2024-03-02 17:59:52 +0400

    created docker file, docker-compose.yml, make file

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payLoad := jsonResponse{
@@ -17,8 +10,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broke",
 	}
 
-	out, _ := json.MarshalIndent(payLoad, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payLoad)
 }
