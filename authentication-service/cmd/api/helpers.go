a7b346896d99f8badafc8bb85b6a044872142afa
INegmetov <negmetov.i@gmail.com>
2024-03-04 23:12:45 +0400

    added logging service, mongoDB

--- authentication-service/cmd/api/helpers.go
+++ authentication-service/cmd/api/helpers.go
@@ -13,14 +13,18 @@ type jsonResponse struct {
 	Data    any    `json:"data,omitempty"`
 }
 
+// readJSON tries to read the body of a request and converts it into JSON
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
-	maxBytes := 1048576
+	maxBytes := 1048576 // one megabyte
+
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
 	dec := json.NewDecoder(r.Body)
 	err := dec.Decode(data)
 	if err != nil {
 		return err
 	}
+
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
 		return errors.New("body must have only a single JSON value")
